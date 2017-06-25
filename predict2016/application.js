0453af4c2853858436ffc6fd6a3747af0da787e2
Zachary Nagler <zachary.nagler@gmail.com>
2017-06-24 23:37:48 -0400

    Update application.js

--- predict2016/application.js
+++ predict2016/application.js
@@ -146,22 +146,20 @@ function setPredictButton(){
 	  var o = {i1: i1,i2: i2,i3: i3,i4: i4}
 	  console.log(o)
 		
-	$.ajax({
-	  crossOrigin: true,
-	  type : "GET",
-	  dataType: "json",
-	  url: "http://znagler.pythonanywhere.com",
-	  data: o,
-	  success: displayResults
-	});		
 // 	$.ajax({
-// 	  dataType: "jsonp",
-// 	  jsonp: false,
-// 	  jsonpCallback: 'callback',
+// 	  crossOrigin: true,
+// 	  type : "GET",
+// 	  dataType: "json",
 // 	  url: "http://znagler.pythonanywhere.com",
 // 	  data: o,
 // 	  success: displayResults
-// 	});
+// 	});		
+	$.ajax({
+	  dataType: "jsonp",
+	  url: "http://znagler.pythonanywhere.com",
+	  data: o,
+	  success: displayResults
+	});
 // 	  $.getJSON( "http://znagler.pythonanywhere.com",o,displayResults)
 
 	});
