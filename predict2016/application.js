66ed87f0ce83ab2115599b80e7f69f2159deaccf
Zachary Nagler <zachary.nagler@gmail.com>
2023-05-14 15:29:46 -0400

    Update application.js

--- predict2016/application.js
+++ predict2016/application.js
@@ -153,10 +153,11 @@ function setPredictButton(){
 // 	  url: "http://znagler.pythonanywhere.com",
 // 	  data: o,
 // 	  success: displayResults
-// 	});		
+// 	});
+	var queryParams = "i1=" + i1 + "i2=" + i2 + "i3=" + i3 + "i4=" + i4
 	$.ajax({
 	  dataType: "text",
-	  url: "https://hq33siilacynsmmt4rkgewxcoi0optah.lambda-url.us-east-1.on.aws/?message1=test1&message2=test2",
+	  url: "https://hq33siilacynsmmt4rkgewxcoi0optah.lambda-url.us-east-1.on.aws/?message1=test1&" + queryParams,
 	  data: o,
 	  success: displayResults
 	});
